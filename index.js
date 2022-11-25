4ba08b5ef33480b5db4ad8d861dd64026a4a1a02
inns_harsh <hkumarmishra@gmail.com>
2022-11-25 20:03:35 +0530

    A

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-
-const express=require("express");
-const app=express();
-const cookieParser=require("cookie-parser");
-const errorMiddleware=require("./middleware/error");
-const bodyParser = require("body-parser");
-const fileUpload = require("express-fileupload");
-app.use(express.json());
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileUpload());
-
-const cors = require('cors');
-const corsOptions ={
-    origin:true,
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+const app=require("./app");
+const dotenv=require("dotenv");
+const cloudinary = require("cloudinary");
+const connectDatabase=require("./config/database");
 
 
-
-// importing routes
-
-const product=require("./routes/productRoute");
-const user=require("./routes/userRoute");
-const order=require("./routes/orderRoute");
-
-app.use("/api/v1", product);
-app.use("/api/v1", user);
-app.use("/api/v1", order);
+// Handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
+    process.exit(1);
+  });
 
 
-// middleware for error
-app.use(errorMiddleware);
-module.exports=app;
+// config
+dotenv.config({path:"backend/config/config.env"});
+
+// connecting
+connectDatabase();
+const server=app.listen(process.env.PORT, () => {
+        console.log(`Server is working on ${process.env.PORT}`);
+ });
+
+ cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+
+// Unhandled Promise Rejection
+ process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  
+    server.close(() => {
+      process.exit(1);
+    });
+  });
